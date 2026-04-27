304e05c60a4f8367357a85fd04c9f677f7582926
brianbuera <brian.buera.it@gmail.com>
2026-04-27 15:01:03 -0300

    version generica

--- src/service/caso_service.py
+++ src/service/caso_service.py
@@ -1,22 +1,34 @@
 from ..infrastructure import PowerPointClient
 from ..factory import Diapositivas
 from pathlib import Path
+import json
+
 
 
 
 class CasoService ():
     
-    ruta_template = Path("C:/Users/u58667/Desktop/Programacion/FolderLister/templates/template.pptx")
-    videos = None 
+    def __init__(self):
+         self.ruta_template = self.buscar_template()
+
 
     def agregar_videos (self, videos):
-        self.videos =  videos[::-1]
+            self.videos =  videos[::-1]
 
     def crear_nuevo_caso(self):
         aplicacion = PowerPointClient()
         presentacion = aplicacion.open_presentacion(self.ruta_template)
         for v in self.videos:
             Diapositivas.crearDiapositiva(presentacion, v)
+    
+    def buscar_template(self):
+         with open("config.json", "r") as f:
+            data = json.load(f)
+            ruta = data.get("ruta_template", "")
+            if Path(ruta).exists():
+                return ruta
+            else:
+                return None
 
 
 
